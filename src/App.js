d68d2a4c60471a8f217611d400fe036b6d356089
Eusebiu <eusebiu.rimboi04@gmail.com>
2022-09-10 16:26:15 +0300

    Da

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export default function App() {
+    const [date, setDate] = useState(new Date());
+    const [counter, setCounter] = useState(0);
 
-export default App;
+    console.log("rendered"); //allows us to visualize re-renders
+
+    function handleButtonClick() {
+        setDate(new Date());
+        setCounter(counter + 1);
+    }
+
+    return <button onClick={handleButtonClick}>Click me</button>
+} 
+
