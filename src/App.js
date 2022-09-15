dc2837b4884b40228acec9b761212d0a76347b50
Eusebiu <eusebiu.rimboi04@gmail.com>
2022-09-15 18:20:48 +0300

    .

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import React, {useState} from "react";
 
 export default function App() {
-    const [date, setDate] = useState(new Date());
-    const [counter, setCounter] = useState(0);
-
-    console.log("rendered"); //allows us to visualize re-renders
-
-    function handleButtonClick() {
-        setDate(new Date());
-        setCounter(counter + 1);
-    }
-
-    return <button onClick={handleButtonClick}>Click me</button>
-} 
+    const user = <>
+        <h1>Eusebiu</h1>,
+        <p>Romania</p>,
+        <p>Barlad</p>
+    </>
+    return user;
+}
 
