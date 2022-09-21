9e961729c426081b4379081e120996d646428506
Eusebiu <eusebiu.rimboi04@gmail.com>
2022-09-21 21:24:11 +0300

    ok

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
-// import { useState, useEffect } from "react";
+// import { useState, useRef, useEffect } from "react";
 import Row from "./Row.js"
 
 export default function App() {
-    return <Row color="red" />;
+    function generateRandomColor() {
+        const characters = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e','f'];
+        let color = "";
+        for (let i = 0; i < 6; ++i) {
+            const pos = Math.floor(Math.random() * 16);
+            color += characters[pos];
+        }
+        console.log(color);
+        return "#" + color;
+    }
+    const count = Math.floor(Math.random() * 11);
+    const colors = [];
+    for (let i = 0; i < count; ++i) {
+        colors.push(generateRandomColor());
+    }
+    return <>
+        {colors.map((col, idx) => {
+            return <Row key={idx} color={col} />
+        })}
+    </>
 }
--- src/Row.js
+++ src/Row.js
@@ -1,7 +1,5 @@
-import { useRef } from "react";
+
 
 export default function Row(props) {
-    const divRef = useRef(0);
-    divRef.current.style.color = "red";
-    return <div ref={divRef}>Red</div>;
+    return <div style={{backgroundColor: props.color}}>{props.color}</div>;
 }
