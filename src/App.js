b2cbe4aa3d97f92f13ca98b0af7d38673fc9e174
Eusebiu <eusebiu.rimboi04@gmail.com>
2022-10-03 17:42:43 +0300

    da

--- src/App.js
+++ src/App.js
@@ -10,42 +10,38 @@ import Input from "./Input"
 
 export default function App() {
     const [drinks, setDrinks] = useState([]);
-    const [auxDrinks, setAuxDrinks] = useState([]);
+    // const [auxDrinks, setAuxDrinks] = useState([]);
     const {get, loading} = useFetch("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=");
     const [substring, setSubstring] = useState("");
 
     useEffect(() => {
-        get("")
+        get(substring)
         .then(data => {
             setDrinks(data.drinks);
-            setAuxDrinks(data.drinks);
+            // setAuxDrinks(data.drinks);
         })
         .catch(error => console.log(error))
-    }, []);
+    }, [get, substring]);
 
     function handleInputChange(event) {
-        const newSubstring = event.target.value;
-        const newDrinks = drinks.filter(drink => {
-            if (drink.strDrink.includes(newSubstring)) {
-                return drink;
-            }
-        })
-        setAuxDrinks(newDrinks);
-        setSubstring(newSubstring);
+        setSubstring(event.target.value);
     }
-    // To do: ingredients, logic, organisation, error message, revision
 
     return (<>
         <Navbar />
-        <Input substring={substring} onInputChange={handleInputChange} />
         <Routes>
-            <Route path="/cocktails" element={loading ? <Loader /> : <>
-                <Cocktails drinks={auxDrinks} />
+            <Route path="/cocktails" element={<>
+                {loading && <Loader />}
+                {!loading && <Input substring={substring} onInputChange={handleInputChange} />}
+                {!loading && <Cocktails drinks={drinks} />}
             </>} />
             <Route path="/about" element={<>
                 <About />
             </>} />
-            <Route path="/cocktails/:id" element={loading ? <Loader /> : <CocktailDetails drinks={auxDrinks} />} />
+            <Route path="/cocktails/:id" element={<>
+                {loading && <Loader />}
+                {!loading && <CocktailDetails drinks={drinks} />}
+            </>} />
         </Routes>
     </>)
 }
